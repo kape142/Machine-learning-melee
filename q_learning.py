a311eca4ff9c872a123c71544a43e36d6d293d6e
Titan2502 <espewaa@stud.ntnu.no>
2018-11-02 13:41:23 +0100

    Changed datatype to float32 for loaded files and all calculated values

--- q_learning.py
+++ q_learning.py
@@ -11,8 +11,10 @@ class Qlearning:
         # Initialize the q table with shape = shape_q_table
         shape_q_table = (self.env.observation_space.high - self.env.observation_space.low + 1).tolist()
         shape_q_table.append(self.env.action_space.n)
-        self.q_table = np.zeros(shape_q_table)
+        self.q_table = np.zeros(shape_q_table, dtype=np.float32)
+
         print("Shape of the Q-table:", self.q_table.shape)
+        print("Datatype of Q-table:", self.q_table.dtype)
 
         # Parameters for the Q alogrithm
         self.alpha = learning_rate
@@ -20,9 +22,9 @@ class Qlearning:
 
         # Global variable for all methods
         self.gamma =  0.9
-        self.max_epsilon = 0.99
+        self.max_epsilon = 1.0
         self.min_epsilon = 0.01
-        self.decay_rate = 0.01
+        self.decay_rate = 0.005
 
         self.all_epochs = []
 
@@ -46,6 +48,7 @@ class Qlearning:
                     actions["action{0}".format(idx+1)] = np.ndarray.argmax(self.q_table[states])
                     if actions["action{0}".format(idx+1)] != 0:
                         print("Bot {0}: ".format(idx+1), "Epoch:", epochs,"Reward: ",reward, "Action: ", actions["action{0}".format(idx+1)])
+            # Get the next state and reward with current aciton
             next_state, reward, done, _ = self.env.step(actions["action1"], actions["action2"])
 
             # Want the next_state on the from [(x,y,z),(x,y,z)] with integeres
@@ -55,37 +58,39 @@ class Qlearning:
                 state_action = state[idx] + (actions["action{0}".format(idx+1)],)
 
                 # Update Q_table for both bots
-                self.q_table[state_action] = self.q_table[state_action] + self.alpha * (reward[idx] + self.gamma*next_max - self.q_table[state_action])
+                self.q_table[state_action] = self.q_table[state_action] + np.float32(self.alpha * (reward[idx] + self.gamma*next_max - self.q_table[state_action]))
 
             state = next_state
             epochs += 1
 
         done = False
 
-        # ------ Inkluderer senere når jeg tar med flere episoder(flere game over) ------ #
-        #self.epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-0.1*self.epsilon)
+        # Oppdaterer epsilon. Blir mindre og mindre (mer og mer avhengig av Q_tabellen)
         self.epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-self.decay_rate*self.epsilon)
+
+
         # Lagrer Q_tabellen
         np.save('q_table_v3.npy', self.q_table)
+        print("Datatype of Q-table after learning:", self.q_table.dtype)
         print("Q table saved to file 'q_table_v3.npy'")
 
 
 if __name__ == '__main__':
     bot = None
-    epsilon = 1.0
+    epsilon = 0.01
     load_old_qtable = True
     try:
         for i in range(10000):
-            print("Iteration: ", i+1)
+            print("============ ITERATION: {0} ============".format(i+1))
             bot = MeleeBot(iso_path="/home/espen/Documents/TTAT3025-ML/LibMelee/Machine-learning-melee/melee.iso", player_control=False)  # change to your path to melee v1.02 NTSC ISO
             # print("Action space: ", bot.action_space.n)
             # print("Observation space: ", bot.observation_space.shape)
             # print("Epoch, reward og actions blir bare printet hvis action ut fra Q_table er noe annet enn 0! Vill skje mer flittig senere ut i treningen")
-            ql = Qlearning(0.5,epsilon, bot)
+            ql = Qlearning(0.3, epsilon, bot)
 
             if load_old_qtable:
-                ql.q_table = np.load('q_table_v3.npy')
-
+                ql.q_table = np.load('q_table_v3.npy').astype(dtype=np.float32)
+                #print("Type of loaded q_table: ", ql.q_table.dtype)
             bot.reset()
             while bot.CheckGameStatus == False:
                 action = bot.action_space.sample()
@@ -98,6 +103,7 @@ if __name__ == '__main__':
             time.sleep(0.5)
             bot.dolphin.terminate()
             time.sleep(1)
+            print("\n============ ITERATION END ============\n\n")
     except Exception as e:
         print(e)
         bot.dolphin.terminate()
